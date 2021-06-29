55caf517b31151ccd2922d2304d50d953c5186b2
msuryawanshi20 <msuryawanshi@rootstock.com>
2021-06-30 03:35:05 +0530

    Project To Project Transfer Template changes for other transactions.

--- src/pageobjects/rstk__Manufacturing.java
+++ src/pageobjects/rstk__Manufacturing.java
@@ -110,6 +110,10 @@ public class rstk__Manufacturing {
 	@LinkType()
 	@FindBy(xpath = "//a[normalize-space(.)='Division to Division Transfer']")
 	public WebElement divisionToDivisionTransfer;
+
+	@LinkType()
+	@FindBy(linkText = "Project to Project Transfer")
+	public WebElement projectToProjectTransfer;
   
  
 
