86ec42382db5696dfc85fb5b5ff1d8ecc3f945ef
achoudhary5501 <62293526+achoudhary5501@users.noreply.github.com>
2021-08-18 18:46:14 +0530

    qarsf migration and sydata test cases

---
+++ src/pageobjects/rstk__CreateNewInvoice.java
@@ -0,0 +1,35 @@
+package pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.provar.core.testapi.annotations.*;
+
+@SalesforcePage( title="Rstk__ Create New Invoice"                                
+               , summary=""
+               , page="CreateNewInvoice"
+               , namespacePrefix="rstk"
+               , object="rstk__soinv__c"
+               , connection="QARSF_Admin"
+     )             
+public class rstk__CreateNewInvoice {
+
+	@TextType()
+	@FindBy(xpath = "//input[@id='soinv_custno__c_autocomplete']")
+	public WebElement Customer;
+	
+	@BooleanType()
+	@FindBy(xpath = "//input[contains(@name,'manualInvoiceSection')]")
+	public WebElement ManualInvoicechkbox;
+	
+	@ButtonType()
+	@FindBy(xpath = "//input[@value='Create Invoice']")
+	public WebElement CreateInvoice;
+	
+	@TextType()
+	@FindBy(id = "li-0")
+	public WebElement Customer_Select;
+	
+}
