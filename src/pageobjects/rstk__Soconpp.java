fc20e6c115fcd216ceb07dd51934cf477c4aff98
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-06-08 02:10:30 +0530

    changes related to labor booking and CPQ issues|added Test Plan for System Setup Behalf of pragati|System Setup changes

--- src/pageobjects/rstk__Soconpp.java
+++ src/pageobjects/rstk__Soconpp.java
@@ -1,8 +1,12 @@
 package pageobjects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,7 +14,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.provar.core.testapi.annotations.ButtonType;
 import com.provar.core.testapi.annotations.ChoiceListType;
 import com.provar.core.testapi.annotations.FindByLabel;
-import com.provar.core.testapi.annotations.PageWaitAfter;
 import com.provar.core.testapi.annotations.SalesforcePage;
 import com.provar.core.testapi.annotations.TextType;
 
@@ -22,29 +25,33 @@ public class rstk__Soconpp {
 		this.driver = driver;
 	}
 
-	/*
-	 * public void selectProduct(String Product) throws InterruptedException {
-	 * Thread.sleep(1000); WebElement ele =
-	 * driver.findElement(By.xpath("//input[@name='soconpp_soprod__c_autocomplete']"
-	 * )); ele.sendKeys(Product); Thread.sleep(2000);
-	 * 
-	 * Actions actions = new Actions(driver); Thread.sleep(500); List<WebElement>
-	 * autoCompleteList = driver
-	 * .findElements(By.xpath("//div[@class='ac_results'][1]/ul[@id='IDREF']/li"));
-	 * for (int i = 0; i < autoCompleteList.size(); i++) { Thread.sleep(1000);
-	 * actions.moveToElement(autoCompleteList.get(i)).build().perform(); if
-	 * (autoCompleteList.get(i).getText().startsWith(Product)) {
-	 * actions.moveToElement(autoCompleteList.get(i)).click().build().perform();
-	 * break; } } }
-	 */
+	
+	public void selectProduct(String Product) throws InterruptedException {
+		Thread.sleep(1000);
+		WebElement ele = driver.findElement(By.xpath("//input[@name='soconpp_soprod__c_autocomplete']"));
+		ele.sendKeys(Product);
+		Thread.sleep(2000);
+
+		Actions actions = new Actions(driver);
+		Thread.sleep(500);
+		List<WebElement> autoCompleteList = driver
+				.findElements(By.xpath("//div[@class='ac_results'][1]/ul[@id='IDREF']/li"));
+		for (int i = 0; i < autoCompleteList.size(); i++) {
+			Thread.sleep(1000);
+			actions.moveToElement(autoCompleteList.get(i)).build().perform();
+			if (autoCompleteList.get(i).getText().startsWith(Product)) {
+				actions.moveToElement(autoCompleteList.get(i)).click().build().perform();
+				Thread.sleep(2000);
+				break;
+			}
+		}
+	}
 	
 	
-	@PageWaitAfter.Field(field = "list", timeoutSeconds = 10)
 	@TextType()
 	@FindBy(xpath = "//input[@name='soconpp_soprod__c_autocomplete']")
 	public WebElement product;
 	
-	@PageWaitAfter.BackgroundActivity(timeoutSeconds = 60)
 	@TextType()
 	@FindBy(xpath = "//li[@id='li-0']")
 	public WebElement list;
@@ -57,6 +64,7 @@ public class rstk__Soconpp {
 		String xpath= "//input[contains(@id,'soconpp_commitqty__c')]";
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
 		driver.findElement(By.xpath(xpath)).sendKeys(""+Qty);
+		driver.findElement(By.xpath(xpath)).sendKeys(Keys.TAB);
 		Thread.sleep(3000);
 	
 	}
@@ -83,7 +91,6 @@ public class rstk__Soconpp {
 	@FindBy(xpath = "//label[normalize-space(.)='Activation Grace Period (Days)']/ancestor::th/following-sibling::td//input")
 	public WebElement activationGracePeriodDays;
 
-	@PageWaitAfter.BackgroundActivity(timeoutSeconds = 60)
 	@ChoiceListType()
 	@FindBy(xpath = "//label[normalize-space(.)='Associated Recurring Charge Product']/ancestor::th/following-sibling::td//select")
 	public WebElement soconpp_rcprod__c;
