5b5341e1e7b890df644706c2dca8a54a8ea70df4
Mayur Suryawanshi <msuryawanshi@rootstock.com>
2022-10-18 01:39:12 +0530

    Misc changes

--- src/pageobjects/rstk__Syumcf.java
+++ src/pageobjects/rstk__Syumcf.java
@@ -1,10 +1,7 @@
 package pageobjects;
 
-import java.util.List;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import com.provar.core.testapi.annotations.*;
 
 @SalesforcePage( title="Rstk__ Syumcf"                                
@@ -19,21 +16,27 @@ public class rstk__Syumcf {
 	@ChoiceListType()
 	@FindBy(xpath = "//span/div/div/select")
 	public WebElement Conversion_Type;
+	
 	@ChoiceListType()
 	@FindBy(xpath = "//label[normalize-space(.)='From UOM']/parent::span/parent::th/following-sibling::td[1]//select")
 	public WebElement From_UOM;
+	
 	@ChoiceListType()
 	@FindBy(xpath = "//label[normalize-space(.)='To UOM']/parent::span/parent::th/following-sibling::td//select")
 	public WebElement To_UOM;
+	
 	@TextType()
 	@FindBy(xpath = "//span/div/div/input")
 	public WebElement unitOfMeasureConversionFactor;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[1]/table//td[2]/span/input[1]")
 	public WebElement save;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[1]/table//td[2]/span/input[1]")
 	public WebElement Edit;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[1]/table//span/input[2]")
 	public WebElement delete;
--- src/pageobjects/rstk__sylocnum.java
+++ src/pageobjects/rstk__sylocnum.java
@@ -1,10 +1,7 @@
 package pageobjects;
 
-import java.util.List;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
 import com.provar.core.testapi.annotations.*;
 
 @SalesforcePage( title="Rstk__sylocnum"                                
@@ -19,21 +16,27 @@ public class rstk__sylocnum {
 	@ChoiceListType()
 	@FindBy(xpath = "//span//select")
 	public WebElement Inventory_Loc_ID;
+	
 	@TextType()
 	@FindBy(xpath = "//label[normalize-space(.)='Inventory Location Number']/parent::span/parent::th/following-sibling::td//input")
 	public WebElement inventoryLocationNumber;
+	
 	@TextType()
 	@FindBy(xpath = "//span/div/div/input")
 	public WebElement description;
+	
 	@TextType()
 	@FindBy(xpath = "//label[normalize-space(.)='Pick Sequence']/parent::span/parent::th/following-sibling::td//input")
 	public WebElement pickSequence;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[1]/table//td[2]/span/input[1]")
 	public WebElement save;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[1]/table//td[2]/span/input[1]")
 	public WebElement Edit;
+	
 	@ButtonType()
 	@FindBy(xpath = "//div[1]/table//span/input[2]")
 	public WebElement delete;
